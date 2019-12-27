96a284c246e52f8f86cddeaf33d9492611fd8471
刘健 <liujian08@tojoy.com>
2019-12-27 11:31:04 +0800

    feat 添加redux

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import { Provider } from 'react-redux'
 import { renderRoutes } from 'react-router-config'
 import { HashRouter } from 'react-router-dom'
+import store from './store'
 import routes from './routes'
 import { IconStyle } from './assets/iconfont/iconfont';
 import { GlobalStyle } from './style';
 
 function App() {
   return (
-    <HashRouter>
-      <GlobalStyle></GlobalStyle>
-      <IconStyle></IconStyle>
-      {
-        renderRoutes(routes)
-      }
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        {
+          renderRoutes(routes)
+        }
+      </HashRouter>
+    </Provider>
   );
 }
 
